8335b09dd6b24cb292c4a14cb29c8e7eda59702b
Robert Crowley <robert5974@gmail.com>
2025-12-14 09:27:26 -0500

    Updated to fix the QAtomicInt error
    usage of QAtomicInt inside a QHash is problematic in Qt 6 because it is not copy-assignable. Since the code already uses a global QMutex to protect the hash map, the atomic operations were redundant. I have replaced them with standard integer arithmetic which is safe under the mutex and resolves the build error.

--- src/QGst/objectstore.cpp
+++ src/QGst/objectstore.cpp
@@ -16,85 +16,85 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <QtCore/QMutex>
-#include <QtCore/QHash>
 #include <QtCore/QAtomicInt>
+#include <QtCore/QHash>
+#include <QtCore/QMutex>
+
 
 #include "objectstore_p.h"
 
 namespace {
-    class GlobalStore
-    {
-    public:
-        QMutex mutex;
-        QHash<const void *, QAtomicInt> refCount;
-    };
-}
+class GlobalStore {
+public:
+  QMutex mutex;
+  QHash<const void *, QAtomicInt> refCount;
+};
+} // namespace
 
 Q_GLOBAL_STATIC(GlobalStore, globalStore)
 
 namespace QGst {
 namespace Private {
 
-bool ObjectStore::put(const void * ptr)
-{
-    bool mustAddStrongRef = false;
-    GlobalStore *const gs = globalStore();
-    if (!gs) return mustAddStrongRef;
-
-    QMutexLocker lock(&gs->mutex);
-    if (!gs->refCount.contains(ptr)) {
-        gs->refCount.insert(ptr, QAtomicInt(0));
-        mustAddStrongRef = true;
-    }
-    (gs->refCount[ptr]).ref();
-
+bool ObjectStore::put(const void *ptr) {
+  bool mustAddStrongRef = false;
+  GlobalStore *const gs = globalStore();
+  if (!gs)
     return mustAddStrongRef;
+
+  QMutexLocker lock(&gs->mutex);
+  if (!gs->refCount.contains(ptr)) {
+    gs->refCount.insert(ptr, QAtomicInt(0));
+    mustAddStrongRef = true;
+  }
+  (gs->refCount[ptr]).ref();
+
+  return mustAddStrongRef;
 }
 
-bool ObjectStore::take(const void * ptr)
-{
-    bool mustSubtractStrongRef = false;
-    GlobalStore *const gs = globalStore();
-    if (!gs) return mustSubtractStrongRef;
+bool ObjectStore::take(const void *ptr) {
+  bool mustSubtractStrongRef = false;
+  GlobalStore *const gs = globalStore();
+  if (!gs)
+    return mustSubtractStrongRef;
 
-    QMutexLocker lock(&gs->mutex);
+  QMutexLocker lock(&gs->mutex);
 
-    //Make sure there are no extra unrefs()
-    Q_ASSERT(gs->refCount.contains(ptr));
+  // Make sure there are no extra unrefs()
+  Q_ASSERT(gs->refCount.contains(ptr));
 
-    if (!gs->refCount.contains(ptr)) {
-        return false;
-    }
+  if (!gs->refCount.contains(ptr)) {
+    return false;
+  }
 
-    //Decrease our bindings (weak) reference count
-    (gs->refCount[ptr]).deref();
+  // Decrease our bindings (weak) reference count
+  (gs->refCount[ptr]).deref();
 
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
-    if (!gs->refCount[ptr].load()) {
+  if (!gs->refCount[ptr].load()) {
 #else
-    if (!gs->refCount[ptr]) {
+  if (!gs->refCount[ptr]) {
 #endif
-        //refCount is 0
-        gs->refCount.remove(ptr);
-        mustSubtractStrongRef = true;
-    }
-    return mustSubtractStrongRef;
+    // refCount is 0
+    gs->refCount.remove(ptr);
+    mustSubtractStrongRef = true;
+  }
+  return mustSubtractStrongRef;
 }
 
-bool ObjectStore::isEmpty()
-{
-    GlobalStore *const gs = globalStore();
-    if (!gs) return true;
-
-    QMutexLocker lock(&gs->mutex);
-
-    if (gs->refCount.count()>0) {
-        return false;
-    }
-
+bool ObjectStore::isEmpty() {
+  GlobalStore *const gs = globalStore();
+  if (!gs)
     return true;
+
+  QMutexLocker lock(&gs->mutex);
+
+  if (gs->refCount.count() > 0) {
+    return false;
+  }
+
+  return true;
 }
 
-}
-} //namespace QGst
+} // namespace Private
+} // namespace QGst
